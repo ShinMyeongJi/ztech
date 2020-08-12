b8979df463c1cc2c1f55a719c1255cb9fe277541
ShinMyeongJi <shinmj1207@gmail.com>
2020-08-12 23:03:13 +0900

    코드 리팩토링

--- backend/src/controllers/lists.js
+++ backend/src/controllers/lists.js
@@ -1,22 +1,18 @@
 
 var express = require('express');
 var mysql = require('mysql');
-var data = require('../../data.json')
-//var dbconfig = require('../../database.json')
-//var connection = mysql.createConnection(dbconfig)
+var dbconfig = require('../../database.js');
+var connection = mysql.createConnection(dbconfig);
 
 var router = express.Router();
 
-
 router.get('/', function(req, res) {
-  /*connection.query('SELECT * FROM stock_info_tbl', (err, results, fields)=>{
+  connection.query('SELECT * FROM stock_info_tbl', (err, results, fields)=>{
     if(err) console.log(err)
     else{
       res.send(results);
     }
-  })*/
-
-  res.send(data);
+  })
 });
 
 module.exports = router;
