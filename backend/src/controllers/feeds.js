747216c988619024ee1c29a6a3c040b4f542fa84
ShinMyeongJi <shinmj1207@gmail.com>
2020-09-06 22:52:53 +0900

    file upload를 위한 aws s3 세팅 작업 중

--- backend/src/controllers/feeds.js
+++ backend/src/controllers/feeds.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var feedInfo = require('../../models').feedInfo;
+
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const fs = require('fs')
+const path = require('path')
+const aws = require('aws-sdk')
+
+aws.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.REGION
+})
+
 var router = express.Router();
+
+const upload = multer({
+  storage : multerS3({
+    s3: s3,
+    bucket: 'imagesforztech',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      console.log(file)
+      cb(null, file.originalname);
+    }
+  })
+})
 
 router.get('/', async(req, res)=> {
   try{
@@ -10,5 +36,17 @@ router.get('/', async(req, res)=> {
     console.error(e);
   }
 });
+
+router.post('/upload', upload.single('img'), (req, res) => {
+  try{
+    console.log("req.file: ", req.file);
+
+    let payLoad = {url : req.file.location};
+    response(res, 200, payLoad);
+  }catch (e) {
+    console.log(e);
+    response(res, 500, "서버 에러")
+  }
+})
 
 module.exports = router;
