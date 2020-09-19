d80d1f6bbc12b1924fd8d9d9289c49cd901d731c
ShinMyeongJi <shinmj1207@gmail.com>
2020-09-20 00:06:24 +0900

    sequelize 수정

---
+++ backend/models/index.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+console.log(config);
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.users = require('./users')(sequelize, Sequelize);
+db.stockInfo = require('./stockInfo')(sequelize, Sequelize);
+db.feedInfo = require('./feedInfo')(sequelize, Sequelize);
+db.feedComment = require('./feedComment')(sequelize, Sequelize);
+
+db.users.hasMany(db.feedInfo, {
+  foreignKey : "name",
+  targetKey : "user_id"
+});
+
+db.feedInfo.belongsTo(db.users, {
+  foreignKey : "name",
+  targetKey : "user_id"
+});
+
+db.users.hasMany(db.feedComment, {
+  foreignKey : "user_name",
+  targetKey : "name"
+})
+
+db.feedInfo.hasMany(db.feedComment,{
+  foreignKey : "feed_id",
+  targetKey : "feed_id"
+})
+
+/*db.sequelize.sync({force : true});*/
+module.exports = db;
+
