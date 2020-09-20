4386fd5eaaac9123862e45427bfdb6970df30c1a
ShinMyeongJi <shinmj1207@gmail.com>
2020-09-20 23:45:45 +0900

    댓글 기능 개발 중

--- backend/src/controllers/feeds.js
+++ backend/src/controllers/feeds.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var feedInfo = require('../../models').feedInfo;
+var feedComment = require('../../models').feedComment;
 
 const multer = require('multer')
 const multerS3 = require('multer-s3')
